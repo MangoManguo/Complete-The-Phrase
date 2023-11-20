2b8460d75f3049a2d34d6df0469552d83bc7ae78
MangoManguo <145375315+MangoManguo@users.noreply.github.com>
2023-11-20 10:12:06 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+// Program Title
+console.log("hi");
+
+// Event listeners
+document.getElementById("btn1").addEventListener("click", function1);
+document.getElementById("btn2").addEventListener("click", function2);
+document.getElementById("btn3").addEventListener("click", function3);
+document.getElementById("btn4").addEventListener("click", function4);
+document.getElementById("btn5").addEventListener("click", function5);
+document.getElementById("btn6").addEventListener("click", function6);
+document.getElementById("btn7").addEventListener("click", function7);
+document.getElementById("btn8").addEventListener("click", function8);
+document.getElementById("btn9").addEventListener("click", function9);
+document.getElementById("btn10").addEventListener("click", function10);
+
+// Event functions
+function function1() {
+  alert("devil.");
+}
+function function2() {
+  alert("short.");
+}
+function function3() {
+  alert("so good.");
+}
+function function4() {
+  alert("your sleeve.");
+}
+function function5() {
+  alert("cake.");
+}
+function function6() {
+  alert("blue moon.");
+}
+function function7() {
+  alert("spilled milk.");
+}
+function function8() {
+  alert("square.");
+}
+function function9() {
+  alert("fall far from the tree.");
+}
+function function10() {
+  alert("chase.");
+}
